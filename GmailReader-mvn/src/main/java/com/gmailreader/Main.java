4fb4a138a265592ab8b258488534b4fc922cc00d
Shashwatsingh22 <shashwatsingh71@gmail.com>
2022-10-18 11:40:31 +0530

    Gmail Init

---
+++ GmailReader-mvn/src/main/java/com/gmailreader/Main.java
@@ -0,0 +1,29 @@
+package com.gmailreader;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+     GmailSer objGmailSer = new GmailSer();
+
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Enter the Querry (like subject:interview)");
+
+        String query = scn.nextLine();
+        HashMap<String, String> data = objGmailSer.getGmailData(query);
+        System.out.println(data.get("subject"));
+        System.out.println("------- Body ---------");
+        System.out.println(data.get("body"));
+        System.out.println("------ Links --------------");
+        System.out.println(data.get("link"));
+
+        System.out.println(">>>>> Total Number of Emails recived by the Mail: <<<<");
+        System.out.println("Total count of emails is :"+objGmailSer.getTotalCountOfMails());
+
+        System.out.println("---- any link present -----");
+        boolean exist = objGmailSer.isMailExist("new link");
+        System.out.println("title exist or not: " + exist);
+
+    }
+}
